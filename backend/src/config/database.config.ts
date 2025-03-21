42957c416691b89c5aff5ade01acbbb4a75c5e72
Vedang <vedang.wappnet@gmail.com>
2025-03-21 21:12:46 +0530

    changed the database config

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,12 +1,16 @@
-import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
-export default registerAs('database', () => ({
+dotenv.config();
+
+export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST ,
+  host: process.env.DATABASE_HOST,
   port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME ,
-  password: process.env.DATABASE_PASSWORD ,
-  database: process.env.DATABASE_NAME ,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: process.env.NODE_ENV !== 'production',
-})); 
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/**/*.entity{.ts,.js'],
+  synchronize: true,
+  logging: true
+};
