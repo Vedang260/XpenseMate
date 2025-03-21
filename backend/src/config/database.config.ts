4b4cfd42abf1efb5753669a6041fcd6a2f657a78
Vedang <vedang.wappnet@gmail.com>
2025-03-21 15:04:10 +0530

    database config is done

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// setting up database config using typeorm
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    autoLoadEntities: true,
+    synchronize: true,
+    logging: true
+}
