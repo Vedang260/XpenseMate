4525f36ab9aaca14f0350af188787ea40e6e2a68
Vedang <vedang.wappnet@gmail.com>
2025-03-21 21:19:34 +0530

    changed the roles

--- backend/src/auth/guards/roles.guard.ts
+++ backend/src/auth/guards/roles.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from '../../common/enums/roles.enum';
+import { UserRole } from '../../common/enums/roles.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
