871a7731609a6c32d6075f4eee87ce7f0c60dbb9
Vedang <vedang.wappnet@gmail.com>
2025-03-21 15:05:50 +0530

    added typeorm config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
 })
 export class AppModule {}
