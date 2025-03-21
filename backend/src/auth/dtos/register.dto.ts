cd75c7d971fcc7d81271764038667bc40ca8da15
Vedang <vedang.wappnet@gmail.com>
2025-03-21 15:35:39 +0530

    register dto is created

---
+++ backend/src/auth/dtos/register.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, MinLength, MaxLength, IsStrongPassword, IsString, IsEmail  } from 'class-validator';
+
+// register dto
+export class RegisterDto {
+    @IsNotEmpty()
+    username: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsStrongPassword()
+    @MinLength(6)
+    @MaxLength(15)
+    password: string;
+}
