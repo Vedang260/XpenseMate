5c342584ae16cc01a81cf62024b947035341849a
Vedang <vedang.wappnet@gmail.com>
2025-03-22 16:29:29 +0530

    category enum is created

---
+++ backend/src/common/enums/category.enum.ts
@@ -0,0 +1,12 @@
+export enum Category {
+    FOOD = 'Food',
+    TRAVEL = 'Travel',
+    SHOPPING = 'Shopping',
+    ENTERTAINMENT = 'Entertainment',
+    RENT = 'Rent',
+    GROCERY = 'Grocery',
+    MEDICAL = 'Medical',
+    FUEL = 'Fuel',
+    PERSONAL_CARE = 'Personal Care',
+    OTHERS = 'Others'
+}
--- backend/src/expenses/entities/expense.entity.ts
+++ backend/src/expenses/entities/expense.entity.ts
@@ -1,3 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Category } from '../../common/enums/category.enum';
+
+@Entity({ name: 'expenses'})
 export class Expense{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 100})
+    title: string;
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    amount: number;
+
+    @Column({ type: 'date' })
+    date: Date;
+
+    @Column({ type: 'enum', enum: Category})
+    category: Category;
+
+    @Column({ length: 500, nullable: true })
+    description?: string;
     
+    @Column({ type: 'enum', enum: PaymentMethod })
+    paymentMethod: PaymentMethod;
+
 }
