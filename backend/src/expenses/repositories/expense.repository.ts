ac2b49673bff968c41918bab732f1e6cfb2922c3
Vedang <vedang.wappnet@gmail.com>
2025-03-22 17:48:19 +0530

    CRUD operations are performed

---
+++ backend/src/expenses/repositories/expense.repository.ts
@@ -0,0 +1,61 @@
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { Expense } from "../entities/expense.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class ExpenseRepository{
+    constructor(
+        @InjectRepository(Expense)
+        private readonly expenseRepository: Repository<Expense>,
+    ) {}
+
+    async getExpenseById(id: number): Promise<Expense | null> {
+        try{
+            const expense = await this.expenseRepository.findOne({ where: { id } });
+            return expense;
+        }catch(error){
+            console.error('Error in retrieving expense: ', error.message);
+            throw new InternalServerErrorException('Error retrieving expense');
+        }
+    }
+
+    async getAllExpenses(user_id: number): Promise<Expense[]>{
+        try{
+            const expenses = this.expenseRepository.find({ where: { user_id }});
+            return expenses;
+        }catch(error){
+            console.error('Error in retrieving all Expense: ', error.message);
+            throw new InternalServerErrorException('Error in retrieving Expenses');   
+        }
+    }
+    async createExpense(expenseData: Partial<Expense>): Promise<Expense> {
+        try{
+            const expense = this.expenseRepository.create(expenseData);
+            return await this.expenseRepository.save(expense);
+        }catch(error){
+            console.error('Error in creating a new Expense: ', error.message);
+            throw new InternalServerErrorException('Error in creating Expense');
+        }
+    }
+
+    async updateExpense(id: number, updatedData: Partial<Expense>): Promise<Expense | null> {
+        try{
+            await this.expenseRepository.update(id, updatedData);
+            return this.getExpenseById(id);
+        }catch(error){
+            console.error('Error in updating expense: ', error.message);
+            throw new InternalServerErrorException('Error in updating expense');
+        }
+    }
+
+    async deleteExpense(id: number): Promise<boolean>{
+        try{
+            const result = await this.expenseRepository.delete(id);
+            return result.affected === 1;
+        }catch(error){
+            console.error('Error in deleting an expense: ', error.message);
+            throw new InternalServerErrorException('Error in deleting expense');
+        }
+    }
+}
